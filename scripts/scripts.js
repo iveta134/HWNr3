d14338708e83cac7e4ad16bdb272204049d1ccbd
Iveta Jaunzema <iveta_jaunzema@inbox.lv>
2020-02-14 01:52:33 +0200

    added button onclick

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,33 @@
 console.log('Script started');
 
 
+function onResetButtonClick() {
+    console.log("button was clicked");
+    let element = document.querySelector(".div-container");
+    while (element.firstchild) {
+        element.removeChild(element.firstchild);
+    }
+}
+
+delButton.onclick = onResetButtonClick;
+
+function onRefreshButtonClick() {
+    console.log("button was clicked");
+
+}
+
+myButton.onclick = onRefreshButtonClick;
+
+function onFizzButtonClick() {
+    console.log("button was clicked");
+
+}
+
+myButton.onclick = onFizzButtonClick;
+
+function onBuzzButtonClick() {
+    console.log("button was clicked");
+
+}
+
+myButton.onclick = onBuzzButtonClick;
